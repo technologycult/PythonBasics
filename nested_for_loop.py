41e191ec1091eb5aaab02523afa817aecfb3f4ff
Imran Khan <36164219+technologycult@users.noreply.github.com>
2019-04-21 17:19:43 +0530

    Session 13
    Nested For Loop

---
+++ nested_for_loop.py
@@ -0,0 +1,18 @@
+'''
+Python 3 Basics - Session # 13
+
+Topic to be covered - Nested For Loops in Python 3
+'''
+'''
+0 0 0
+x 0 2
+y 0 3
+z 0 4
+'''
+
+for x in range(3):
+    for y in range(4):
+        for z in range(5):
+            print(x,y,z)
+
+
